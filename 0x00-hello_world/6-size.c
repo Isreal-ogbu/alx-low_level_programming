dee98c8d4753e0b8dc074f186177e8025e763c72
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-12 16:37:50 +0100

    size of a computer variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,14 @@
 
 /**
  * main - print the size of a variable in a computer
- * 
  * return: 0 if computed properly, otherwise non zero
  */
-int main()
+int main(void)
 {
-        printf("size of a char : %d\n",sizeof(char));
-        printf("size of a int : %d\n",sizeof(int));
-        printf("size of a long int : %d\n",sizeof(long int));
-        printf("size of a long long long int : %d\n",sizeof(long long int));
-        printf("size of a float : %d\n",sizeof(float));
+        printf("size of a char : %d byte(s)\n",sizeof(char));
+        printf("size of a int : %d byte(s)\n",sizeof(int));
+        printf("size of a long int : %d (byte)\n",sizeof(long int));
+        printf("size of a long long long int : %d (byte)\n",sizeof(long long int));
+        printf("size of a float : %d byte(s)\n",sizeof(float));
         return (0);
 }
