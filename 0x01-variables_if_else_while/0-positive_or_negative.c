54e3509d149900d8c3588223cd46fcf7be4e9c86
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-14 13:26:48 +0100

    checking for positive, negative, or zero and sending out the output

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main : to test random variable of n if positive or negative
+ * 
+ */
+int main(void)
+{
+    int n;
+    printf("%d\n" &n)
+    if (n < 0) {
+        printf("%d - is negative\n", n);
+    }
+    else if ( n == 0) {
+        printf("%d - is zero\n");
+    }
+    else {
+        print("%d - positive\n");
+    }
+
+}
