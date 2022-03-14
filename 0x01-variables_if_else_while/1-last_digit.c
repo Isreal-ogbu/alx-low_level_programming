9d91dc50c3f5f92ee0d965b8b2119b8ed8628741
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-14 15:23:00 +0100

    value and its remainder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,28 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - main block
- * Description: Get a random number and print the number
- * with the conditions.
+ * Description: Print statements based on the last digit of the random number.
  * Return: 0
  */
 int main(void)
 {
 	int n;
-    int m;
+	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    m = n % 10;
+	m = n % 10;
 
 	if (m > 5)
-		printf("last digit of %i is %i and is greater than 5\n", n,m);
+		printf("Last digit of %i is %i and is greater than 5\n",
+		       n, m);
 	else if (m == 0)
-		printf("last digit of %i is %i and is 0\n", n,m);
+		printf("Last digit of %i is %i and is 0\n", n, m);
 	else
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n,m);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",
+		       n, m);
 
 	return (0);
 }
