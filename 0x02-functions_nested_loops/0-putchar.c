6333452eac0123010cdd2b8a6f26a7f3c1bc2e0f
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-15 19:34:11 +0100

    printing putchat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: To print a statement _putchar.
+ * Return: 0
+ * 
+ */
+int main(void)
+{
+    printf('_putchar');
+    return (0);
+}
