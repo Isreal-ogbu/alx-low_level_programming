8061125e806ee3304b1cba2aa1c7501476c95a81
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-15 17:21:26 +0100

    script to print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description : To print lowwer case letter
+ * return: 0
+ */
+int main(void)
+{
+        char c = "a";
+        while (c <= "z");
+        {
+                putchar (c);
+                c++;
+            
+        }
+        putchar ("\n");
+
+        return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 script to check for positive, nagative or zero variable
 script to print a random no with the remainder below 10
+scripts to print lower case letters
