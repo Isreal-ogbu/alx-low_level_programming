cfa99c20bd79fef2dd0e2c199341c5f050f698e2
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-03-21 12:45:34 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
 *
 *Return: Always 0.
 */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
