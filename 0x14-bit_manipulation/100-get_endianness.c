08128a06827501857499769c7bc98bc3b62a6238
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-05-01 11:44:58 +0100

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/** 
+  * Write a C program to find out if the underlying
+  * @n: decimal number
+  * @index: index of the bit
+  * Return: If an error occurs - -1.
+  * Otherwise - 1. 
+ */
+int get_endianness(void);
+{
+  unsigned int x = 0x76543210;
+  char *c = (char*) &x;
+ 
+  printf ("*c is: 0x%x\n", *c);
+  if (*c == 0x10)
+  {
+    printf ("Underlying architecture is little endian. \n");
+  }
+  else
+  {
+     printf ("Underlying architecture is big endian. \n");
+  }
+ 
+  return 0;
+}
